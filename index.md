9285062cdfc21ae2ec07d9c6ca2a654355b9b72a
emmanuel daniels <73504340+ed55567@users.noreply.github.com>
2021-08-27 09:58:10 -0400

    added audio files
    added audio test_1  mp3

--- index.md
+++ index.md
@@ -1,8 +1,1 @@
-<div id="hud">
-    <h1>Exploring Computer Science</h1>
-    <h2>Computer science is everywhere! Computing technologies impact life.</h2>
-  </div>  
-  <button class="myButton1">Computer Science</button>
-  <button class="myButton2">Computer Scientist</button>
-  <button class="myButton3">Impacts of Computing</button>
-</div>
+2_4 Computer Science
--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('script.js loaded');
+
+document.getElementById("saveform").addEventListener("click", myFunction);
+       function myFunction() {
+         window.location.href="http://programminghead.com";
+       }
+
+
+document.getElementById("saveform1").addEventListener("click", myFunction);
+        function myFunction() {
+          window.location.href="http://programminghead.com";
+        }
+
+
+document.getElementById("saveform2").addEventListener("click", myFunction);
+        function myFunction() {
+          window.location.href="http://programminghead.com";
+        }
