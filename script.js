47ca7c0de47b044cebace5a2dcc85f4d891f2c38
emmanuel daniels <73504340+ed55567@users.noreply.github.com>
2021-08-28 12:21:36 -0400

    updated styles.css
    remove display none

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ console.log('script.js loaded');
 
 document.getElementById("saveform").addEventListener("click", myFunction);
        function myFunction() {
-         window.location.href="http://programminghead.com";
+         window.location.href="https://instructional-resources.s3.amazonaws.com/PLTW_Launch/tempfolder/2_4_Computer_Science/computer_science.html";
        }
 
 
 document.getElementById("saveform1").addEventListener("click", myFunction);
         function myFunction() {
-          window.location.href="http://programminghead.com";
+          window.location.href="https://instructional-resources.s3.amazonaws.com/PLTW_Launch/tempfolder/2_4_Computer_Science/computer_scientist.html";
         }
 
 
 document.getElementById("saveform2").addEventListener("click", myFunction);
         function myFunction() {
-          window.location.href="http://programminghead.com";
+          window.location.href="https://instructional-resources.s3.amazonaws.com/PLTW_Launch/tempfolder/2_4_Computer_Science/impact_of_computing.html";
         }
